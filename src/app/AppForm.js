7096e7a62171802ba2e84ab768c27ce0b6dc4177
C Tidd <contact@ctidd.com>
2016-11-21 19:54:34 -0600

    Clean up AppForm.

--- src/app/AppForm.js
+++ src/app/AppForm.js
@@ -4,12 +4,8 @@ import Field from '../ui/field/Field';
 export default ({ fields, onFieldChange }) => (
     <form>
         <ul className="atui-vList atui-vList_sm">
-            { fields
-                .toArray()
-                .sort((a, b) => a.index - b.index)
-                .map((field, i) => (
+            { fields.map((field, i) => (
                 <li key={ field.get('token') }>
-                    { console.log(field.get('token')) }
                     <Field
                         id={ field.get('token') }
                         label={ field.get('label') }
@@ -17,7 +13,7 @@ export default ({ fields, onFieldChange }) => (
                         onChange={ onFieldChange.bind(field.get('token')) }
                         />
                 </li>
-            )) }
+            )).toArray() }
         </ul>
     </form>
 );
