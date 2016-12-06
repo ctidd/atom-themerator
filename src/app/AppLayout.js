fffe44e5662d6d862123dc035adfd77dc14876cf
C Tidd <contact@ctidd.com>
2016-12-06 06:05:36 -0600

    Implement rough styles for file upload button.

--- src/app/AppLayout.js
+++ src/app/AppLayout.js
@@ -22,12 +22,23 @@ export default ({
                     </h1>
                     <p>Atom Themerator is an online syntax theme generator for Atom. Prepare your syntax theme with live preview, then download the generated theme for use &mdash; or manually tweak the generated theme for more precise styling.</p>
                     <p><a href="https://github.com/ctidd/atom-themerator">Atom Themerator on GitHub</a></p>
-                    <div>
-                        <label htmlFor="theme-restore" className="atui-hdg atui-hdg_minor">
-                            Modifying an existing Atom Themerator theme? Upload its atom-themerator.json file:
+                </div>
+                <div className="atui-box atui-box_dusty">
+                    <div className="atui-field">
+                        <h2 className="atui-hdg atui-hdg_minor">
+                            Modifying an existing Atom Themerator theme? Start by uploading its atom-themerator.json file:
+                        </h2>
+                        <input
+                            id="theme-restore"
+                            className="atui-field-input atui-field-input_file"
+                            type="file"
+                            onChange={ onRestore } />
+                        <label
+                            className="atui-field-pseudoFile"
+                            htmlFor="theme-restore">
+                            Select Config File
                         </label>
                     </div>
-                    <input id="theme-restore" type="file" onChange={ onRestore } />
                 </div>
                 <AppForm
                     snippet={ snippet }
